1eba56c17fe1bc1bb50ea0d30fda26991dcc1a0b
saidurrahmanmisket <mdsaidurrahmanmisket@gmail.com>
2023-07-13 03:03:45 +0600

    comment upddate

--- resources/views/components/comment.blade.php
+++ resources/views/components/comment.blade.php
@@ -8,10 +8,10 @@
                 <button type="submit" class="btn btn-primary my-3">Submit</button>
             </form>
             <!-- Comment with nested comments -->
-            <div id="commenter">
+            <div id="oldComment">
 
             </div>
-            <div id="oldComment">
+            <div id="commenter">
 
             </div>
 
@@ -44,62 +44,52 @@
                 let post = await axios.post(url, data);
                 document.getElementById('comment').value = '';
 
-                  let firstUrl = '/firstComment';
-            let firstResult = await axios.get(firstUrl);
-            if (200 === firstResult.status) {
-                const thisPostId = window.location.pathname.split('/').pop();
-
-                if (firstResult.data.postID == thisPostId) {
-
-
-                    document.getElementById('commenter').innerHTML =
-                        (`
-                        
+                document.getElementById('commenter').innerHTML =
+                    (`
                         <div class="d-flex mb-4">
                         <div class="flex-shrink-0"><img class="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
                         <div class="ms-3">
                         <div class="fw-bold">Commenter Name</div>
-                        ${firstResult.data.comment}
+                        ${comment}
                         </div>
                         </div>
                         
                         `)
 
 
-                }
 
-            };
 
+
+                document.getElementById('commentForm').addEventListener('submit', async (event) => {
+                    event.preventDefault();
+
+                    document.getElementById('oldComment').innerHTML +=
+                        (`
+                        
+                        <div class="d-flex mb-4">
+                        <div class="flex-shrink-0"><img class="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
+                        <div class="ms-3">
+                        <div class="fw-bold">Commenter Name</div>
+                        ${comment}
+                        </div>
+                        </div>
+                        
+                        `)
+
+
+
+                })
             } catch (error) {
                 console.log(error);
             }
-
-          
-
-
         })
 
-        // if (201 == post.status) {
+        
 
-        //         document.getElementById('commenter').innerHTML =
-        //             (`
+    }
 
-        //     <div class="d-flex mb-4">
-        //     <div class="flex-shrink-0"><img class="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
-        //     <div class="ms-3">
-        //         <div class="fw-bold">Commenter Name</div>
-        //             ${comment}
-        //         </div>
-        // </div>
-
-        //     `)
-
-        //         document.getElementById('comment').value = '';
-
-
-
-
-
+    newFunc()
+    async function newFunc(){
         let getUrl = '/getComments';
         let result = await axios.get(getUrl);
         if (200 === result.status) {
@@ -124,42 +114,6 @@
                 }
             })
         }
-        // }
-
-
-
-
-        // // old comment 
-        // try {
-
-        //     let getUrl = '/getComments';
-        //     let result = await axios.get(getUrl);
-        //     if (200 === result.status) {
-        //         result.data.forEach((item) => {
-        //             const thisPostId = window.location.pathname.split('/').pop();
-
-        //             if (item.postID == thisPostId) {
-
-
-        //                 document.getElementById('oldComment').innerHTML +=
-        //                     (`
-
-        //                 <div class="d-flex mb-4">
-        //                 <div class="flex-shrink-0"><img class="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
-        //                 <div class="ms-3">
-        //                 <div class="fw-bold">Commenter Name</div>
-        //                 ${item.comment}
-        //                 </div>
-        //                 </div>
-
-        //                 `)
-        //             }
-        //         })
-        //     }
-        // } catch (error) {
-        //     console.log(error);
-        // }
-
 
     }
 </script>
